adb369dafa2d9b58339e34883663193b91c22eb6
miguel0201 <mmitsuishi@netprotections.co.jp>
2024-05-05 18:18:52 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+function reportMainSize() {
+  const info = document.querySelector('main > .info');
+  const main = document.getElementById('clamped');
+  info.textContent = `width: ${main.offsetWidth}`
+}
+
+function reportWindowSize() {
+  const info = document.querySelector('.container > .info');
+  info.textContent = `window width: ${window.innerWidth}`
+}
+
+function highlightAppliedWidth() {
+  const min = document.querySelector('span.min');
+  const val = document.querySelector('span.val');
+  const max = document.querySelector('span.max');
+
+  const width = window.innerWidth;
+  if (width > 1200) {
+    val.classList.remove('highlight');
+    max.classList.add('highlight');
+  } else if (width < 600) {
+    min.classList.add('highlight');
+    val.classList.remove('highlight');
+  } else {
+    min.classList.remove('highlight');
+    val.classList.add('highlight');
+    max.classList.remove('highlight');
+  }
+}
+
+window.addEventListener("load", reportMainSize);
+window.addEventListener("load", reportWindowSize);
+window.addEventListener("load", highlightAppliedWidth);
+
+window.addEventListener("resize", reportMainSize);
+window.addEventListener("resize", reportWindowSize);
+window.addEventListener("resize", highlightAppliedWidth);
+
